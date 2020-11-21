28619220e319c2c173596b6a3183f56d72ef166c
SofaSergeeva <64421088+SofaSergeeva@users.noreply.github.com>
2020-11-21 17:47:31 +0300

    Counts new position with distance to external face

--- CPP_functions/NewPosition/NewPositions.h
+++ CPP_functions/NewPosition/NewPositions.h
@@ -1,11 +1,23 @@
 #ifndef NEWPOSITION_NEWPOSITIONS_H
 #define NEWPOSITION_NEWPOSITIONS_H
+
+#include <iostream>
 #include <vector>
+#include <map>
+#include <unordered_set>
+#include <queue>
 #include "math.h"
-#include <utility>
+
+std::map<int, int> CalculateDistances(const std::vector<std::pair<int, int>> &graph,
+                                      const std::vector<int> &external_face);
 
 std::pair<int, int>
-NewPosition(std::pair<int, int> cur_pos, const std::vector<std::pair<int, int>> &neighbours, double cool, double C);
+NewPosition(std::pair<int, std::pair<int, int>> cur_vertex, // index, position
+            const std::vector<std::pair<int, std::pair<int, int>>> &neighbours,
+            const std::vector<std::pair<int, int>> &graph,
+            const std::vector<int> &external_face,
+            int cool,
+            int A);
 
 
 #endif //NEWPOSITION_NEWPOSITIONS_H
